055178cf0168713ca10d2d890d9c0b92c9a8a4df
Yueluo <qgy123@users.noreply.github.com>
2018-06-17 03:26:18 +0800

    Add files via upload

---
+++ Native.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetEaseDcNpPlugin
+{
+    public class Native
+    {
+        public static IntPtr GetNetEaseWindowText(int processId)
+        {
+            IntPtr mainWindowHandle = IntPtr.Zero;
+
+            EnumWindows((hWnd, lParam) =>
+            {
+                GetWindowThreadProcessId(hWnd, out var pid);
+
+                if (pid == lParam)
+                {
+                    StringBuilder str = new StringBuilder(256);
+                    GetClassName(hWnd, str, 256);
+
+                    if (str.ToString() == "OrpheusBrowserHost")
+                    {
+                        mainWindowHandle = hWnd;
+                        return false;
+                    }
+                }
+
+                return true;
+
+            }, new IntPtr(processId));
+
+            return mainWindowHandle;
+        }
+
+        internal const uint GW_OWNER = 4;
+
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        private static extern int GetWindowThreadProcessId(IntPtr hWnd, out IntPtr lpdwProcessId);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
+
+        [DllImport("User32.Dll")]
+        private static extern void GetClassName(IntPtr hwnd, StringBuilder s, int nMaxCount);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextW(IntPtr hwnd, StringBuilder text, int maxLength);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Timers;
+
+namespace NetEaseDcNpPlugin
+{
+    public class Program
+    {
+        private static DiscordRpc.RichPresence _presence = new DiscordRpc.RichPresence();
+        private static DiscordRpc.EventHandlers _handlers = new DiscordRpc.EventHandlers();
+
+        private static string _lastPlaying = "";
+
+        static void Main(string[] args)
+        {
+            //RegCallBack();
+
+            Timer timer = new Timer(1000);
+            timer.Elapsed += tick;
+            timer.Start();
+
+            Console.ReadLine();
+        }
+
+        private static void tick(object sender, ElapsedEventArgs e)
+        {
+            Update();
+        }
+
+        private static void RegCallBack()
+        {
+            _handlers.readyCallback += ReadyCallback;
+            _handlers.disconnectedCallback += DisconnectedCallback;
+            _handlers.errorCallback += ErrorCallback;
+        }
+
+        private static void Update()
+        {
+            DiscordRpc.Initialize(Const.ApplicationId, ref _handlers, false, null);
+
+            string now = GetNetEaseNowPlaying(GetNetEaseProcessId());
+
+            if (!string.IsNullOrWhiteSpace(now))
+            {
+                _presence.details = "Now Listening:";
+                _presence.state = now;
+                _presence.largeImageKey = "icon";
+                _presence.largeImageText = "NetEaseMusic";
+                _presence.startTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+                if (_lastPlaying != now)
+                {
+                    _lastPlaying = now;
+                    DiscordRpc.UpdatePresence(_presence);
+                    Console.WriteLine(now);
+                }
+
+            }
+
+
+        }
+
+        private static int GetNetEaseProcessId()
+        {
+            Process[] processes = Process.GetProcessesByName("cloudmusic");
+            int pid = 0;
+
+            foreach (var process in processes)
+            {
+                if (string.IsNullOrWhiteSpace(process.StartInfo.Arguments))
+                {
+                    pid = process.Id;
+                }
+            }
+
+            return pid;
+        }
+
+        private static string GetNetEaseNowPlaying(int pid)
+        {
+            StringBuilder text = new StringBuilder(256);
+
+            if (pid != 0)
+            {
+                IntPtr hwnd = Native.GetNetEaseWindowText(pid);
+                if (hwnd != IntPtr.Zero)
+                {
+                    Native.GetWindowTextW(hwnd, text, Native.GetWindowTextLength(hwnd) + 1);
+                }
+            }
+
+            return text.ToString();
+        }
+
+        private static void ErrorCallback(int errorCode, string message)
+        {
+            Console.WriteLine("ErrorCallback : " + errorCode);
+        }
+
+        private static void DisconnectedCallback(int errorCode, string message)
+        {
+            Console.WriteLine("Disconnected Callback Error Code : " + errorCode);
+        }
+
+        private static void ReadyCallback(ref DiscordRpc.DiscordUser connectedUser)
+        {
+            Console.WriteLine("Connected!");
+            Console.WriteLine($"User + {connectedUser.username}");
+            Console.WriteLine($"userId + {connectedUser.userId}");
+            Console.WriteLine($"discriminator + {connectedUser.discriminator}");
+        }
+    }
+}
